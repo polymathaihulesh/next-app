0c35d4208632c7d18e0cda1ee864ceeb6a8c419d
huleshjangde <huleshjangde1@gmail.com>
2025-10-06 18:33:35 +0530

    test 9

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ console.log("API URL check:", process.env.NEXT_PUBLIC_API_URL);
  
 
   <div className="flex gap-4 items-center flex-col sm:flex-row">
-    <h1>Hulesh</h1>
+    
         <div className="flex flex-col gap-4 mt-8">
           <button
             className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 disabled:opacity-50"
